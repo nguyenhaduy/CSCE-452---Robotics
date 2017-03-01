9b2ec1c60f393a9e99534e61ad068408a825a31f
Duy Nguyen <nguyenhaduy@tamu.edu>
2017-02-28 18:50:05 -0600

    Finished Project1

--- Project 1/project_1_gui.m
+++ Project 1/project_1_gui.m
@@ -54,21 +54,18 @@ function project_1_gui_OpeningFcn(hObject, eventdata, handles, varargin)
 % varargin   command line arguments to project_1_gui (see VARARGIN)
 
 % Choose default command line output for project_1_gui
-handles.output = hObject;
+    handles.output = hObject;
+    handles.points = [];
 
 % Update handles structure
     guidata(hObject, handles);
 
 %axis off;
-    global T0_1 T1_2 T2_3 T3_4 R1 R2 R3 P0 P1 P2 P3 P4 theta1 theta2 theta3;
+    global T0_1 T1_2 T2_3 T3_4 P0 P1 P2 P3 P4 theta1 theta2 theta3 points;
     T0_1 = [1 0 0 0; 0 1 0 0; 0 0 1 0; 0 0 0 1];
     T1_2 = [1 0 0 0; 0 1 0 150; 0 0 1 0; 0 0 0 1];
-
-    R1 = [1 0 0; 0 1 0; 0 0 1];
     T2_3 = [1 0 0 0; 0 1 0 100; 0 0 1 0; 0 0 0 1];
-    R2 = [1 0 0; 0 1 0; 0 0 1];
     T3_4 = [1 0 0 0; 0 1 0 75; 0 0 1 0; 0 0 0 1];
-    R3 = [1 0 0; 0 1 0; 0 0 1];
     P0 = [0; 0; 0; 1];
     P1 = [0; 0; 0; 1];
     P2 = [0; 150; 0; 1];
@@ -77,16 +74,21 @@ handles.output = hObject;
     theta1 = 0;
     theta2 = 0;
     theta3 = 0;
-
+    points = [];
+    
     cla;
-    xx = [P1(1), P2(1), P3(1), P4(1)];
-    yy = [P1(2), P2(2), P3(2), P4(2)];
-    zz = [0, 0, 0, 0];
-    plot(xx,yy,'.','markersize',30,'Color','r');
-    hold on
-
     xlim([-400 400])
     ylim([0 400])
+    hold on;
+    rectangle('Position',[-15,0,30,10],'FaceColor', 'y');
+    draw_arms;
+    
+   
+    X = [P1(1), P2(1), P3(1), P4(1)];
+    Y = [P1(2), P2(2), P3(2), P4(2)];
+    plot(X,Y,'.','markersize',30,'Color','r');    
+
+    
 % axis([-400 400 0 400])
 
 % UIWAIT makes project_1_gui wait for user response (see UIRESUME)
@@ -109,73 +111,20 @@ function Counter1_Callback(hObject, eventdata, handles) %Counter-Clockwise Joint
 % hObject    handle to Counter1 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-    global T0_1 T1_2 T2_3 T3_4 R1 R2 R3 P0 P1 P2 P3 P4 theta1 theta2 theta3;
-    theta1 = theta1 + 1;
-    R1 = [cosd(theta1),-sind(theta1),0;sind(theta1),cosd(theta1),0;0,0,1]
-    % P2(1:3) = R1 * P2(1:3)
-    for i = 1:3
-        for j = 1:4
-            if (j<4)&&(i<4)
-                T0_1(i,j) = R1(i,j);
-    %         else 
-    %             T1(i,j) = P1(i);
-            end
-        end
-    end
-    T0_1
-
-    P1 = T0_1 * P0
-    P2 = T0_1 * T1_2 * P0
-    P3 = T0_1 * T1_2 * T2_3 * P0
-    P4 = T0_1 * T1_2 * T2_3 * T3_4 * P0
-
-    cla;
-
-    xx = [P1(1), P2(1), P3(1), P4(1)];
-    yy = [P1(2), P2(2), P3(2), P4(2)];
-    zz = [0, 0, 0, 0];
-    plot(xx,yy,'.','markersize',30,'Color','r');
-    hold on
-
-
-% temp = [T2(1,4), T2(2,4), T2(3,4), 1]
-% temp  = temp * P1
-% for i = 1:3
-%     T2(i,4) = temp(1,i)
-% end
-
+global T0_1 T1_2 T2_3 T3_4 P1 P2 P3 P4 theta1 theta2 theta3;
+theta1 = theta1 + 5;
+T0_1 = T_matrix(theta1, T0_1);
+update();
 
 % --- Executes on button press in Clock1.
 function Clock1_Callback(hObject, eventdata, handles) %Clockwise Joint 1
 % hObject    handle to Clock1 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-    global T0_1 T1_2 T2_3 T3_4 R1 R2 R3 P0 P1 P2 P3 P4 theta1 theta2 theta3;
-    theta1 = theta1 - 1;
-    R1 = [cosd(theta1),-sind(theta1),0;sind(theta1),cosd(theta1),0;0,0,1]
-    for i = 1:3
-        for j = 1:4
-            if (j<4)&&(i<4)
-                T0_1(i,j) = R1(i,j);
-    %         else 
-    %             T1(i,j) = P1(i);
-            end
-        end
-    end
-    T0_1
-
-    P1 = T0_1 * P0
-    P2 = T0_1 * T1_2 * P0
-    P3 = T0_1 * T1_2 * T2_3 * P0
-    P4 = T0_1 * T1_2 * T2_3 * T3_4 * P0
-
-    cla;
-
-    xx = [P1(1), P2(1), P3(1), P4(1)];
-    yy = [P1(2), P2(2), P3(2), P4(2)];
-    zz = [0, 0, 0, 0];
-    plot(xx,yy,'.','markersize',30,'Color','r');
-    hold on
+global T0_1 T1_2 T2_3 T3_4 P0 P1 P2 P3 P4 theta1 theta2 theta3;
+theta1 = theta1 - 5;
+T0_1 = T_matrix(theta1, T0_1);
+update();
 
 
 
@@ -184,32 +133,10 @@ function Counter2_Callback(hObject, eventdata, handles) %Counter-Clockwise Joint
 % hObject    handle to Counter2 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-    global T0_1 T1_2 T2_3 T3_4 R1 R2 R3 P0 P1 P2 P3 P4 theta1 theta2 theta3;
-    theta2 = theta2 + 1;
-    R2 = [cosd(theta2),-sind(theta2),0;sind(theta2),cosd(theta2),0;0,0,1]
-    for i = 1:3
-        for j = 1:4
-            if (j<4)&&(i<4)
-                T1_2(i,j) = R2(i,j);
-    %         else 
-    %             T1(i,j) = P1(i);
-            end
-        end
-    end
-    T0_1
-
-    P1 = T0_1 * P0
-    P2 = T0_1 * T1_2 * P0
-    P3 = T0_1 * T1_2 * T2_3 * P0
-    P4 = T0_1 * T1_2 * T2_3 * T3_4 * P0
-
-    cla;
-
-    xx = [P1(1), P2(1), P3(1), P4(1)];
-    yy = [P1(2), P2(2), P3(2), P4(2)];
-    zz = [0, 0, 0, 0];
-    plot(xx,yy,'.','markersize',30,'Color','r');
-    hold on
+global T0_1 T1_2 T2_3 T3_4 P0 P1 P2 P3 P4 theta1 theta2 theta3;
+theta2 = theta2 + 5;
+T1_2 = T_matrix(theta2, T1_2);
+update();
 
 
 % --- Executes on button press in Clock2.
@@ -217,64 +144,20 @@ function Clock2_Callback(hObject, eventdata, handles) %Counter-Clockwise Joint 2
 % hObject    handle to Clock2 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-    global T0_1 T1_2 T2_3 T3_4 R1 R2 R3 P0 P1 P2 P3 P4 theta1 theta2 theta3;
-    theta2 = theta2 - 1;
-    R2 = [cosd(theta2),-sind(theta2),0;sind(theta2),cosd(theta2),0;0,0,1]
-    for i = 1:3
-        for j = 1:4
-            if (j<4)&&(i<4)
-                T1_2(i,j) = R2(i,j);
-    %         else 
-    %             T1(i,j) = P1(i);
-            end
-        end
-    end
-    T0_1
-
-    P1 = T0_1 * P0
-    P2 = T0_1 * T1_2 * P0
-    P3 = T0_1 * T1_2 * T2_3 * P0
-    P4 = T0_1 * T1_2 * T2_3 * T3_4 * P0
-    
-    cla;
-
-    xx = [P1(1), P2(1), P3(1), P4(1)];
-    yy = [P1(2), P2(2), P3(2), P4(2)];
-    zz = [0, 0, 0, 0];
-    plot(xx,yy,'.','markersize',30,'Color','r');
-    hold on
+global T0_1 T1_2 T2_3 T3_4 P0 P1 P2 P3 P4 theta1 theta2 theta3;
+theta2 = theta2 - 5;
+T1_2 = T_matrix(theta2, T1_2);
+update();
 
 % --- Executes on button press in Counter3.
 function Counter3_Callback(hObject, eventdata, handles)
 % hObject    handle to Counter3 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-    global T0_1 T1_2 T2_3 T3_4 R1 R2 R3 P0 P1 P2 P3 P4 theta1 theta2 theta3;
-    theta3 = theta3 + 1;
-    R3 = [cosd(theta3),-sind(theta3),0;sind(theta3),cosd(theta3),0;0,0,1]
-    for i = 1:3
-        for j = 1:4
-            if (j<4)&&(i<4)
-                T2_3(i,j) = R3(i,j);
-    %         else 
-    %             T1(i,j) = P1(i);
-            end
-        end
-    end
-    T0_1
-
-    P1 = T0_1 * P0
-    P2 = T0_1 * T1_2 * P0
-    P3 = T0_1 * T1_2 * T2_3 * P0
-    P4 = T0_1 * T1_2 * T2_3 * T3_4 * P0
-    
-    cla;
-
-    xx = [P1(1), P2(1), P3(1), P4(1)];
-    yy = [P1(2), P2(2), P3(2), P4(2)];
-    zz = [0, 0, 0, 0];
-    plot(xx,yy,'.','markersize',30,'Color','r');
-    hold on
+global T0_1 T1_2 T2_3 T3_4 P0 P1 P2 P3 P4 theta1 theta2 theta3;
+theta3 = theta3 + 5;
+T2_3 = T_matrix(theta3, T2_3);
+update();
 
 
 % --- Executes on button press in Clock3.
@@ -282,52 +165,79 @@ function Clock3_Callback(hObject, eventdata, handles)
 % hObject    handle to Clock3 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-    global T0_1 T1_2 T2_3 T3_4 R1 R2 R3 P0 P1 P2 P3 P4 theta1 theta2 theta3;
-    theta3 = theta3 + 1;
-    R3 = [cosd(theta3),-sind(theta3),0;sind(theta3),cosd(theta3),0;0,0,1]
-    for i = 1:3
-        for j = 1:4
-            if (j<4)&&(i<4)
-                T2_3(i,j) = R3(i,j);
-    %         else 
-    %             T1(i,j) = P1(i);
-            end
-        end
-    end
-    T0_1
-
-    P1 = T0_1 * P0
-    P2 = T0_1 * T1_2 * P0
-    P3 = T0_1 * T1_2 * T2_3 * P0
-    P4 = T0_1 * T1_2 * T2_3 * T3_4 * P0
+global T0_1 T1_2 T2_3 T3_4 P0 P1 P2 P3 P4 theta1 theta2 theta3;
+theta3 = theta3 - 5;
+T2_3 = T_matrix(theta3, T2_3);
+update();
 
 % --- Executes on button press in Paint.
 function Paint_Callback(hObject, eventdata, handles) %Paint
 % hObject    handle to Paint (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+global P4 points
+points = [points, P4];
+update();
+    
 
-function h = generate_Arm(a,b,c)
-%a = horizontal radius
-%b = vertical radius
-%c = color
-beta = 0 * (pi/180);
-sinbeta = sin(beta);
-cosbeta = cos(beta);
-alpha = linspace(0,360,50)' .* (pi/180); %linspace 3rd arg = #steps = smoothness
-sinalpha = sin(alpha);
-cosalpha = cos(alpha);
-X = (a*cosalpha*cosbeta-b*sinalpha*sinbeta);
-Y = (a*cosalpha*sinbeta+b*sinalpha*cosbeta);
-h = fill(X,Y,c);
-set(h,'FaceAlpha',0.75')
+function A = draw_arms(number)
+global T0_1 T1_2 T2_3 T3_4 P0 P1 P2 P3 P4 theta1 theta2 theta3;
 
-function h = generate_Circle(x,y,r,c)
-%x = x loc
-%y = y loc
-%r = radius
-%c = color
-angle = 0:0.01:2*pi;
-xp = r * cos(angle);
-yp = r * sin(angle);
-h = fill(x + xp, y + yp, c);
+alpha = linspace(0,360,50)';
+
+E1 = T0_1;
+E1(:,4) = (P2-P1)/2;
+Arm1 = [20*cosd(alpha) 90*sind(alpha)];
+Arm1(:,4) = 1;
+Arm1 = Arm1';
+Arm1 = E1*Arm1;
+
+E2 = T1_2;
+E2(:,4) = (P3+P2)/2;
+Arm2 = [20*cosd(alpha) 70*sind(alpha)];
+Arm2(:,4) = 1;
+Arm2 = Arm2';
+E1(:,4) = [0; 0; 0; 1];
+Arm2 = E2*E1*Arm2;
+
+E3 = T2_3;
+E3(:,4) = (P4+P3)/2;
+Arm3 = [20*cosd(alpha) 57.5*sind(alpha)];
+Arm3(:,4) = 1;
+Arm3 = Arm3';
+E2(:,4) = [0; 0; 0; 1];
+Arm3 = E3*E2*E1*Arm3;      
+
+plot(Arm1(1,:),Arm1(2,:),'r');
+plot(Arm2(1,:),Arm2(2,:),'b');
+plot(Arm3(1,:),Arm3(2,:),'g');
+
+function T = T_matrix(theta, T)
+% Generate Transformation Matrix
+R = [cosd(theta),-sind(theta),0;sind(theta),cosd(theta),0;0,0,1];
+for i = 1:3
+    for j = 1:3
+        T(i,j) = R(i,j);
+    end
+end
+
+function update()
+%Update all the points on graph
+
+global T0_1 T1_2 T2_3 T3_4 P0 P1 P2 P3 P4 theta1 theta2 theta3 points;
+
+P1 = T0_1 * P0;
+P2 = T0_1 * T1_2 * P0;
+P3 = T0_1 * T1_2 * T2_3 * P0;
+P4 = T0_1 * T1_2 * T2_3 * T3_4 * P0;
+
+cla;
+draw_arms;
+if length(points) ~= 0
+    plot(points(1,:), points(2,:), '.','markersize',10,'Color','k');
+end
+X = [P1(1), P2(1), P3(1), P4(1)];
+Y = [P1(2), P2(2), P3(2), P4(2)];
+plot(X,Y,'.','markersize',30,'Color','r');
+hold on
+
