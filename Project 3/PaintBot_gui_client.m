0f5a867a39e9f9bcf5bcce2e29bc915c67df044a
ktremblay2307 <ktremblay2307@tamu.edu>
2017-04-03 16:44:25 -0500

    Client works now!

--- Project 3/PaintBot_gui_client.m
+++ Project 3/PaintBot_gui_client.m
@@ -1,30 +1,4 @@
 function varargout = PaintBot_gui(varargin)
-% PaintBot_gui MATLAB code for PaintBot_gui.fig
-%      PaintBot_gui, by itself, creates a new PaintBot_gui or raises the existing
-%      singleton*.
-%
-%      H = PaintBot_gui returns the handle to a new PaintBot_gui or the handle to
-%      the existing singleton*.
-%
-%      PaintBot_gui('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in PaintBot_gui.M with the given input arguments.
-%
-%      PaintBot_gui('Property','Value',...) creates a new PaintBot_gui or raises the
-%      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before PaintBot_gui_OpeningFcn gets called.  An
-%      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to PaintBot_gui_OpeningFcn via varargin.
-%
-%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
-%      instance to run (singleton)".
-%
-% See also: GUIDE, GUIDATA, GUIHANDLES
-
-% Edit the above text to modify the response to help PaintBot_gui
-
-% Last Modified by GUIDE v2.5 01-Apr-2017 16:14:26
-
-% Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
@@ -41,27 +15,22 @@ if nargout
 else
     gui_mainfcn(gui_State, varargin{:});
 end
-% End initialization code - DO NOT EDIT
-
+% End initialization code - DO NOT EDIT  
 
 % --- Executes just before PaintBot_gui is made visible.
 function PaintBot_gui_OpeningFcn(hObject, eventdata, handles, varargin)
-% This function has no output args, see OutputFcn.
-% hObject    handle to figure
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to PaintBot_gui (see VARARGIN)
-
-% Choose default command line output for PaintBot_gui
     handles.output = hObject;
     handles.points = [];
 
 % Update handles structure
     guidata(hObject, handles);
 
+
     
 %axis off;
-    global T0_1 T1_2 T2_3 T3_4 P0 P1 P2 P3 P4 theta1 theta2 theta3 points continuousDraw t Connection;
+    global T0_1 T1_2 T2_3 T3_4 P0 P1 P2 P3 P4 theta1 theta2 theta3 points continuousDraw Connection delay;
+    delay = false;
+    Connection = false;
     continuousDraw = 0;
     T0_1 = [1 0 0 0; 0 1 0 0; 0 0 1 0; 0 0 0 1];
     T1_2 = [1 0 0 0; 0 1 0 150; 0 0 1 0; 0 0 0 1];
@@ -83,101 +52,11 @@ function PaintBot_gui_OpeningFcn(hObject, eventdata, handles, varargin)
     hold on;
     update();
     
-    Connection = false;
-        
     set(gca, 'box','off','XTickLabel',[],'XTick',[],'YTickLabel',[],'YTick',[])
-    
-    
-% axis([-400 400 0 400])
-
-% UIWAIT makes PaintBot_gui wait for user response (see UIRESUME)
-% uiwait(handles.figure1);
-
 
 % --- Outputs from this function are returned to the command line.
 function varargout = PaintBot_gui_OutputFcn(hObject, eventdata, handles) 
-% varargout  cell array for returning output args (see VARARGOUT);
-% hObject    handle to figure
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Get default command line output from handles structure
 varargout{1} = handles.output;
-
-
-% --- Executes on button press in Counter1.
-function Counter1_Callback(hObject, eventdata, handles) %Counter-Clockwise Joint 1
-% hObject    handle to Counter1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-global T0_1 T1_2 T2_3 T3_4 P1 P2 P3 P4 theta1 theta2 theta3;
-theta1 = theta1 + 2
-T0_1 = T_matrix(theta1, T0_1);
-update();
-
-% --- Executes on button press in Clock1.
-function Clock1_Callback(hObject, eventdata, handles) %Clockwise Joint 1
-% hObject    handle to Clock1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-global T0_1 T1_2 T2_3 T3_4 P0 P1 P2 P3 P4 theta1 theta2 theta3;
-theta1 = theta1 - 2
-T0_1 = T_matrix(theta1, T0_1);
-update();
-
-
-
-% --- Executes on button press in Counter2.
-function Counter2_Callback(hObject, eventdata, handles) %Counter-Clockwise Joint 2
-% hObject    handle to Counter2 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-global T0_1 T1_2 T2_3 T3_4 P0 P1 P2 P3 P4 theta1 theta2 theta3;
-theta2 = theta2 + 2;
-T1_2 = T_matrix(theta2, T1_2);
-update();
-
-
-% --- Executes on button press in Clock2.
-function Clock2_Callback(hObject, eventdata, handles) %Counter-Clockwise Joint 2
-% hObject    handle to Clock2 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-global T0_1 T1_2 T2_3 T3_4 P0 P1 P2 P3 P4 theta1 theta2 theta3;
-theta2 = theta2 - 2;
-T1_2 = T_matrix(theta2, T1_2);
-update();
-
-% --- Executes on button press in Counter3.
-function Counter3_Callback(hObject, eventdata, handles)
-% hObject    handle to Counter3 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-global T0_1 T1_2 T2_3 T3_4 P0 P1 P2 P3 P4 theta1 theta2 theta3;
-theta3 = theta3 + 2;
-T2_3 = T_matrix(theta3, T2_3);
-update();
-
-
-% --- Executes on button press in Clock3.
-function Clock3_Callback(hObject, eventdata, handles)
-% hObject    handle to Clock3 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-global T0_1 T1_2 T2_3 T3_4 P0 P1 P2 P3 P4 theta1 theta2 theta3;
-theta3 = theta3 - 2;
-T2_3 = T_matrix(theta3, T2_3);
-update();
-
-% --- Executes on button press in Paint.
-function Paint_Callback(hObject, eventdata, handles) %Paint
-% hObject    handle to Paint (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-global P4 points
-points = [points, P4];
-update();
-    
 
 function A = draw_arms(number)
 global T0_1 T1_2 T2_3 T3_4 P0 P1 P2 P3 P4 theta1 theta2 theta3;
@@ -243,45 +122,6 @@ if (continuousDraw == 1)
     points = [points, P4];
 end
 hold on
-
-
-% --- Executes on button press in UpButton.
-function UpButton_Callback(hObject, eventdata, handles)
-% hObject    handle to UpButton (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-global T0_1 T1_2 T2_3 T3_4 P0 P1 P2 P3 P4 theta1 theta2 theta3 points;
-disp('Up')
-inverseKine(P4(1), P4(2)+5);
-
-
-% --- Executes on button press in RightButton.
-function RightButton_Callback(hObject, eventdata, handles)
-% hObject    handle to RightButton (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-global T0_1 T1_2 T2_3 T3_4 P0 P1 P2 P3 P4 theta1 theta2 theta3 points;
-disp('Right')
-inverseKine(P4(1)+5, P4(2));
-
-% --- Executes on button press in LeftButton.
-function LeftButton_Callback(hObject, eventdata, handles)
-% hObject    handle to LeftButton (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-global T0_1 T1_2 T2_3 T3_4 P0 P1 P2 P3 P4 theta1 theta2 theta3 points;
-disp('Left')
-inverseKine(P4(1)-5, P4(2));
-
-
-% --- Executes on button press in DownButton.
-function DownButton_Callback(hObject, eventdata, handles)
-% hObject    handle to DownButton (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-global T0_1 T1_2 T2_3 T3_4 P0 P1 P2 P3 P4 theta1 theta2 theta3 points;
-disp('Down')
-inverseKine(P4(1), P4(2)-5);
 
 % Link angle calculation
 function inverseKine(x,y)
@@ -374,132 +214,70 @@ T0_1 = T_matrix(theta1, T0_1);
 
 update();
 
-function ContinuousPaint_Callback(hObject, eventdata, handles)
-% hObject    handle to ContinuousPaint (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-global T0_1 T1_2 T2_3 T3_4 P0 P1 P2 P3 P4 theta1 theta2 theta3 points continuousDraw;
-continuousDraw = ~continuousDraw;
-
-function message = client(host, port, number_of_retries)
-
-    import java.net.Socket
-    import java.io.*
-
-    if (nargin < 3)
-        number_of_retries = 20; % set to -1 for infinite
-    end
-    
-    retry        = 0;
-    input_socket = [];
-    message      = [];
-
-    while true
-
-        retry = retry + 1;
-        if ((number_of_retries > 0) && (retry > number_of_retries))
-            fprintf(1, 'Too many retries\n');
-            break;
-        end
-        
-        try
-            fprintf(1, 'Retry %d connecting to %s:%d\n', ...
-                    retry, host, port);
-
-            % throws if unable to connect
-            input_socket = Socket(host, port);
-
-            % get a buffered data input stream from the socket
-            input_stream   = input_socket.getInputStream;
-            d_input_stream = DataInputStream(input_stream);
-
-            fprintf(1, 'Connected to server\n');
-
-            % read data from the socket - wait a short time first
-            pause(0.5);
-            bytes_available = input_stream.available;
-            fprintf(1, 'Reading %d bytes\n', bytes_available);
-            
-            message = zeros(1, bytes_available, 'uint8');
-            for i = 1:bytes_available
-                message(i) = d_input_stream.readByte;
-            end
-            
-            message = char(message);
-            
-            % cleanup
-            input_socket.close;
-            break;
-            
-        catch
-            if ~isempty(input_socket)
-                input_socket.close;
-            end
-
-            % pause before retrying
-            pause(1);
-        end
-    end
-
-function serverName_Callback(hObject, eventdata, handles) %insert server name
-% hObject    handle to serverName (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'String') returns contents of serverName as text
-%        str2double(get(hObject,'String')) returns contents of serverName as a double
-
-
-% --- Executes during object creation, after setting all properties.
-function serverName_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to serverName (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: edit controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
-
-% --- Executes on button press in connect. 
 function connect_Callback(hObject, eventdata, handles)
 % hObject    handle to connect (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global Connection
+global T0_1 T1_2 T2_3 T3_4 P1 P2 P3 P4 theta1 theta2 theta3 Connection points continuousDraw delay;
 Connection = true;
-t = tcpip('127.0.0.1',4013);
+t = tcpip('10.202.148.74',4013);
 fopen(t);
 pause(0.2);
 
 t.Terminator = ' ';
     
 while Connection
+    if(delay)
+        pause(4)
+    end
         DataReceived=fscanf(t,'%c')
         if (strcmpi(DataReceived, 'DOWN '))
+            inverseKine(P4(1), P4(2)-5);
             % Call down callback function
             
         elseif (strcmpi(DataReceived, 'UP '))
+            inverseKine(P4(1), P4(2)+5);
             % Call up callback function
             
         elseif (strcmpi(DataReceived, 'LEFT '))
+            inverseKine(P4(1)-5, P4(2))
             % Call left callback function
-            
         elseif (strcmpi(DataReceived, 'RIGHT '))
-            % Call right callback function
-            
+            inverseKine(P4(1)+5, P4(2));
+            % Call right callback function       
+        elseif (strcmpi(DataReceived, 'CLOCK1 '))
+            theta1 = theta1 - 2;
+            T0_1 = T_matrix(theta1, T0_1);
+            update();
+        elseif (strcmpi(DataReceived, 'CLOCK2 '))
+            theta2 = theta2 - 2;
+            T1_2 = T_matrix(theta2, T1_2);
+            update();
+        elseif (strcmpi(DataReceived, 'CLOCK3 '))
+            theta3 = theta3 - 2;
+            T2_3 = T_matrix(theta3, T2_3);
+            update();
+        elseif (strcmpi(DataReceived, 'COUNTER1 '))
+            theta1 = theta1 + 2;
+            T0_1 = T_matrix(theta1, T0_1);
+            update();
+        elseif(strcmpi(DataReceived, 'COUNTER2 '))
+            theta2 = theta2 + 2;
+            T1_2 = T_matrix(theta2, T1_2);
+            update();
+        elseif(strcmpi(DataReceived, 'COUNTER3 '))
+            theta3 = theta3 + 2;
+            T2_3 = T_matrix(theta3, T2_3);
+            update();
+        elseif(strcmpi(DataReceived, 'PAINT '))
+            points = [points, P4];
+            update();
+        elseif(strcmpi(DataReceived, 'CONTINUOUS '))
+            continuousDraw = ~continuousDraw;
+        elseif (strcmpi(DataReceived, 'DISCONNECT '))
+            Connection = false;
+        elseif (strcmpi(DataReceived, 'DELAY '))
+            delay = ~delay;
         end
+        drawnow;
 end
-
-
-% --- Executes on button press in disconnect. 
-function disconnect_Callback(hObject, eventdata, handles)
-% hObject    handle to disconnect (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-global Connection
-Connection = false;
-fclose(t);
-delete(t);
